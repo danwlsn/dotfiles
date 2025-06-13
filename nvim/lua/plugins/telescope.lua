b370c1eea0b8db64dcbcdb8fa784c6288413fb9e
Danny Wilson <dwlsn@pm.me>
2025-06-13 10:47:38 +0100

    clean up telescope keys

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -8,8 +8,11 @@ return {
   },
   keys = {
     { '<leader>.', function() require('telescope.builtin').find_files() end,  desc = 'Telescope find files' },
+    { '<leader>b', function() require('telescope.builtin').buffers() end, desc = 'Telescope buffers' },
     { '<leader>fg', function() require('telescope.builtin').live_grep() end, desc = 'Telescope live grep' },
-    { '<leader>bb', function() require('telescope.builtin').buffers() end, desc = 'Telescope buffers' },
     { '<leader>fh', function() require('telescope.builtin').help_tags() end, desc = 'Telescope help tags' },
+    { '<leader>fq', function() require('telescope.builtin').quickfix() end, desc = 'Telescope quickfix' },
+    { '<leader>fr', function() require('telescope.builtin').lsp_references() end, desc = 'Telescope LSP Reference' },
+    { '<leader>fd', function() require('telescope.builtin').lsp_definition() end, desc = 'Telescope LSP Definition' },
   }
 }
