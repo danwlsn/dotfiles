6ab3dc282cdc4f70d5aa57e6ddb3c037ebd51895
Danny Wilson <dwlsn@pm.me>
2025-10-14 11:56:36 +0100

    avante and claude

--- nvim/lua/plugins/avante.lua
+++ nvim/lua/plugins/avante.lua
@@ -5,6 +5,31 @@ return {
   version = false, -- Never set this value to "*"! Never!
   ---@module 'avante'
   ---@type avante.Config
+  opts = {
+    mode = "legacy", 
+    provider = "claude-code",
+    acp_providers = {
+      ["claude-code"] = {
+        command = "npx",
+        args = { "@zed-industries/claude-code-acp" },
+        env = {
+          NODE_NO_WARNINGS = "1",
+          ANTHROPIC_API_KEY = os.getenv("ANTHROPIC_API_KEY"),
+        },
+      },
+    },
+    providers = {
+      claude = {
+        endpoint = "https://api.anthropic.com",
+        model = "claude-sonnet-4-20250514",
+        timeout = 30000, -- Timeout in milliseconds
+          extra_request_body = {
+            temperature = 0.75,
+            max_tokens = 20480,
+          },
+      },
+    },
+  },
   dependencies = {
     "nvim-lua/plenary.nvim",
     "MunifTanjim/nui.nvim",
